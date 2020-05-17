85942e02e27ed1351337ad096bbd4797cae35ac6
agrochal <artur@procer.pl>
2020-05-17 22:13:42 +0200

    Code refactor
    Simplifying the code and grammar fixes

--- app.js
+++ app.js
@@ -1,61 +1,51 @@
 console.log("Welcome to Welit!\n");
-console.log("Type 'exit' instead of city to stop the program\n");
+console.log("Type 'exit' instead of the city to stop the program.\n");
 
 const config = require('./config');
 const fetch = require('node-fetch');
 const readline = require('readline');
 
-let jsondata;
-
 const rl = readline.createInterface({
-   input: process.stdin,
-   output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 function getInput() {
-   rl.question('Weather for which city do you want to know? ', (answer) => {
-      if (answer == "exit") {
-         rl.close();
-      } else {
-         getData(answer);
-      }
-   });
+  rl.question('Weather for which city do you want to know? ', (answer) => {
+    if (answer == "exit") {
+      rl.close();
+    } else {
+      getData(answer);
+    }
+  });
 }
 
 getInput();
 
 function getData(city) {
-   const reg = /^[A-Za-z]+$/;
-   if ((city.replace(/ /g, "")).match(reg)) {
-      var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + config.api;
-      getWeather(city, url);
-   } else {
-      console.log("\nEnter valid city name!\n");
-      getInput();
-   }
+  const reg = /^[A-Za-z]+$/;
+  if ((city.replace(/ /g, "")).match(reg)) {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${config.api}`;
+    getWeather(city, url);
+  } else {
+    console.log("\nEnter a valid city name!\n");
+    getInput();
+  }
 }
 
-
 function getWeather(city, url) {
-   fetch(url)
-      .then(function(u) {
-         return u.json();
-      }).then(
-         function(json) {
-            jsondata = json;
-            if (jsondata.cod == 200) {
-               var weather = '';
-               for (let i = 0; i < jsondata.weather.length; i++) {
-                  weather += jsondata.weather[i].main + " ";
-                  if (i == jsondata.weather.length - 1) {
-                     console.log("\n" + city + ' | Temp: ' + jsondata.main.temp + '°C | ' + weather + '\n');
-                     getInput();
-                  }
-               }
-            } else {
-               console.log("\nEnter valid city name!\n");
-               getInput();
-            }
-         }
-      )
+  fetch(url)
+    .then(function(u) {
+      return u.json();
+    }).then(
+      function(json) {
+        if (json.cod == 200) {
+          console.log(`\n${city} | Temp: ${json.main.temp}°C | ${json.weather[0].main}\n`);
+          getInput();
+        } else {
+          console.log("\nEnter a valid city name!\n");
+          getInput();
+        }
+      }
+    )
 }
